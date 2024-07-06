a42060aebdbda330b9deb203f4b62afcee1fda35
Anita18P <anita18.palle@gmail.com>
2024-07-06 13:53:28 +0530

    relations between tables

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 const {ObjectId}=require('mongodb');
 const getDb=require('../util/database').getDb;
 class User{
-  constructor(username,email){
+  constructor(username,email,cart,id){
     this.name=username;
     this.email=email;
+    this.cart=cart;
+    this._id=id;
   }
   save(){
     const db=getDb();
@@ -15,6 +17,29 @@ class User{
     // .catch(err=>{
     //   console.log(err);
     // })
+  }
+  addToCart(product){
+    const cartProductIndex=this.cart.items.findIndex(cp=>{
+      return cp.productId.toString()===product._id.toString();
+    })
+    let newQuantity=1;
+    const updatedCartItems=[...this.cart.items];
+
+    if(cartProductIndex>=0){
+      newQuantity=this.cart.items[cartProductIndex].quantity+1;
+      updatedCartItems[cartProductIndex].quantity=newQuantity;
+    }else{
+      updatedCartItems.push({
+        productId:new ObjectId(product._id),
+        quantity:newQuantity})
+    }
+
+    const updatedCart={
+      items:updatedCartItems
+    }
+     const db=getDb();
+    return  db.collection('user').updateOne({_id:new ObjectId(this._id)},{$set:{cart:updatedCart}})
+
   }
 
 static findById(userId){
