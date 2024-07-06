b601bd445226ec5db42ec22a555c98b7b843c4e6
Anita18P <anita18.palle@gmail.com>
2024-07-06 16:14:31 +0530

    getting orders placed

--- models/user.js
+++ models/user.js
@@ -65,6 +65,29 @@ class User{
     return  db.collection('user').updateOne({_id:new ObjectId(this._id)},{$set:{cart:{items:updatedCartItems}}})
 
   }
+   addOrder(){
+    const db=getDb();
+    return this.getCart().then(products=>{
+      const order={
+        items:products,
+        user:{
+          _id:new ObjectId(this._id),
+          name:this.name,
+          email:this.email
+        }
+      }
+        return db.collection('orders').insertOne(order)
+    }) .then(result=>{
+      this.cart={items:[]};
+      return  db.collection('user').updateOne({_id:new ObjectId(this._id)},{$set:{cart:{items:[]}}})
+    })
+   }
+   getOrders(){
+    const db=getDb();
+    return db.collection('orders').find({'user._id':new ObjectId(this._id)})
+    .toArray()
+
+   }
 
 static findById(userId){
   const db=getDb();
