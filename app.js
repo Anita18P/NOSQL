7548c014be52773fc3c96b97cb8b9ee2928b6322
Anita18P <anita18.palle@gmail.com>
2024-07-08 18:50:53 +0530

    creating user table

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-//const User=require('./models/user');
+const User=require('./models/user');
 
 
 const errorController = require('./controllers/error');
@@ -19,15 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('6687fb7d29d6603b20f60982')
-//     .then(user => {
-//       req.user = new User(user.name,user.email,user.cart,user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
+app.use((req, res, next) => {
+  User.findById('668be12661aac67a3fd173e7')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
   
-// });
+});
 
 app.use('/admin', adminRoutes);
  app.use(shopRoutes);
@@ -35,6 +35,20 @@ app.use('/admin', adminRoutes);
 app.use(errorController.get404);
 mongoose.connect('mongodb+srv://anita18palle:JBYNFiCAtLeOlZUW@cluster0.pzu2v0z.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
   .then(result=>{
+    User.findOne().then(user=>{
+      if(!user){
+        const user=new User({
+          name:"Anita",
+          email:"anita18.palle@gmail.com",
+          cart:{
+            items:[]
+          }
+        });
+        user.save();
+
+      }
+    })
+    
     app.listen(3000);
   }).catch(err=>{
     console.log(err);
