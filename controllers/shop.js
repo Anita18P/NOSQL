68004894e0b68c5bdb74d1d850e5760129299d65
Anita18P <anita18.palle@gmail.com>
2024-07-09 13:59:53 +0530

    cart System

--- controllers/shop.js
+++ controllers/shop.js
@@ -42,10 +42,12 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
-exports.getCart = (req, res, next) => {
-  req.user
-    .getCart()
-    .then(products => {
+exports.getCart =async  (req, res, next) => {
+  const y=await req.user
+    .populate('cart.items.productId')
+  .then(user => {
+      console.log(user.cart.items);
+      const products=user.cart.items;
           res.render('shop/cart', {
             path: '/cart',
             pageTitle: 'Your Cart',
